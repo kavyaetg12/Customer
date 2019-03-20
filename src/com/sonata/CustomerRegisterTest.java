d855e22107ecdf5f163ae3759ab59b853cc188d3
unknown <sontrng@BG1WS0373.SONATA.LOCAL>
2019-03-20 13:01:47 +0530

    files are added

---
+++ src/com/sonata/CustomerRegisterTest.java
@@ -0,0 +1,21 @@
+package com.sonata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerRegisterTest {
+
+	@Test
+	public void test1()
+	{
+		String Name="kavya";
+		assertEquals("kavya",name);
+	}
+	@Test
+	public void test2()
+	{
+		String Name="nayana";
+		assertEquals("nayana",name);
+	}
+}
